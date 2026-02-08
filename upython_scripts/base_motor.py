cf0ef6821c4b2fbd7fe1a1051281a67310e406d2
linzhanguca <lzhang12@uca.edu>
2026-02-08 16:13:49 -0600

    update motor dir

--- upython_scripts/base_motor.py
+++ upython_scripts/base_motor.py
@@ -20,12 +20,12 @@ class BaseMotor:
 
     def forward(self, speed=0.0):  # map 0~65535 to 0~1
         assert 0 <= speed <= 1  # make sure speed in range [0, 1]
-        self.phase_pin.on()
+        self.phase_pin.off()
         self.enable_pin.duty_u16(int(65535 * speed))
 
     def backward(self, speed=0.0):  # map 0~65535 to 0~1
         assert 0 <= speed <= 1  # make sure speed in range [0, 1]
-        self.phase_pin.off()
+        self.phase_pin.on()
         self.enable_pin.duty_u16(int(65535 * speed))
 
 
@@ -34,8 +34,8 @@ if __name__ == "__main__":
     from utime import sleep
 
     # SETUP
-    # m = BaseMotor(phase_id=16, enable_id=17, sleep_id=18)  # left motor
-    m = BaseMotor(phase_id=15, enable_id=14, sleep_id=13)  # right motor
+    m = BaseMotor(phase_id=16, enable_id=17, sleep_id=18)  # left motor
+    # m = BaseMotor(phase_id=15, enable_id=14, sleep_id=13)  # right motor
 
     # LOOP
     m.awaken()
